bedc4da1bedc4cb90f1c7090ab70e3b0311b37fa
Anthony Feliciano <anthonyfeliciano@localhost.localdomain>
2020-11-19 16:51:33 -0500

    made canvas and snake head

--- sketch.js
+++ sketch.js
@@ -1,15 +1,42 @@
 
-var blob;
 function setup() {
-
-  createCanvas(600, 600);
-  blob = new Blob();
+  createCanvas(800, 800);
+  s = new Snake(0, 0);
 }
 function draw() {
-
-  background(0);
-  blob.show();
+  background(0, 255, 0);
+  s.update();
+  s.show();
 }
 
+class Snake {
 
-console.log("lets get skecthing boys");
+  constructor() {
+
+    //Keep track of all vectors in snake (have them reference vector in front of them)
+    this.body = [];
+    this.body[0] = createVector(0, 0);
+
+    //keep track of the direction the snake is moving in
+    this.xdir = 1;
+    this.ydir = 0;
+
+  }
+
+  update() {
+    this.body[0].x += this.xdir;
+    this.body[0].y += this.ydir;
+
+
+
+  }
+
+  show() {
+    rect(this.body[0].x, this.body[0].y, 20, 20)
+
+  }
+
+
+
+
+}
