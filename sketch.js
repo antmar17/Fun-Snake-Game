cbae8d21c00ecd9c48c71a4c9ef7643a69acbd36
antmar17 <anthonyrfeliciano17@gmail.com>
2020-11-19 18:05:10 -0500

    made snake ALOT faster

--- sketch.js
+++ sketch.js
@@ -1,14 +1,50 @@
 let s;
+let food;
+
+
+let resolution = 10;
 function setup() {
   createCanvas(800, 800);
-  s = new Snake(0, 0);
-}
-function draw() {
-  background(0, 255, 0);
-  s.update();
-  s.show();
+  frameRate(60);
+
+  s = new Snake();
+  food = createVector(random(), random)
 }
 
+
+
+
+class Snake {
+
+  constructor() {
+
+    //Keep track of all vectors in snake (have them reference vector in front of them)
+    this.body = [];
+    this.body[0] = createVector(0, 0);
+
+    //keep track of the direction the snake is moving in
+    this.xdir = 1;
+    this.ydir = 0;
+
+  }
+
+  update() {
+    this.body[0].x += this.xdir;
+    this.body[0].y += this.ydir;
+
+
+
+  }
+
+  show() {
+    rect(this.body[0].x, this.body[0].y, 1, 1)
+
+  }
+
+
+
+
+}
 
 //changes direction of snake
 function keyPressed() {
@@ -37,34 +73,11 @@ function keyPressed() {
   }
 
 }
-class Snake {
-
-  constructor() {
-
-    //Keep track of all vectors in snake (have them reference vector in front of them)
-    this.body = [];
-    this.body[0] = createVector(0, 0);
-
-    //keep track of the direction the snake is moving in
-    this.xdir = 1;
-    this.ydir = 0;
-
-  }
-
-  update() {
-    this.body[0].x += this.xdir;
-    this.body[0].y += this.ydir;
 
 
-
-  }
-
-  show() {
-    rect(this.body[0].x, this.body[0].y, 20, 20)
-
-  }
-
-
-
-
+function draw() {
+  scale(resolution);
+  background(0, 255, 0);
+  s.update();
+  s.show();
 }
