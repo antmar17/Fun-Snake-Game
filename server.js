4085cfb7580fde9ec2601f08580dd42d05003447
antmar17 <anthonyrfeliciano17@gmail.com>
2020-11-21 08:10:41 -0500

    installed packages for server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+//import express
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
 console.log("my server is running!");
+
+var socket = recquire('socket.io');
+io.sockets.on('connection', newConnection);
+
+//process for new connection
+function newConnection(socket) {
+
+  console.log(socket + "Connected");
+
+}
